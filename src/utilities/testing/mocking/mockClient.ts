d48979c82f91e0355eb1b82960dd3c37ab6b6057
Kevin <kevin.peraza@live.com>
2021-02-27 18:10:28 -0600

    Agregar console.log

--- src/utilities/testing/mocking/mockClient.ts
+++ src/utilities/testing/mocking/mockClient.ts
@@ -1,19 +1,22 @@
-import { DocumentNode } from 'graphql';
+import { DocumentNode } from "graphql";
 
-import { ApolloClient } from '../../../core';
-import { InMemoryCache, NormalizedCacheObject } from '../../../cache';
-import { mockSingleLink } from './mockLink';
+import { ApolloClient } from "../../../core";
+import { InMemoryCache, NormalizedCacheObject } from "../../../cache";
+import { mockSingleLink } from "./mockLink";
 
 export function createMockClient<TData>(
   data: TData,
   query: DocumentNode,
-  variables = {},
+  variables = {}
 ): ApolloClient<NormalizedCacheObject> {
+  console.log(data);
   return new ApolloClient({
     link: mockSingleLink({
       request: { query, variables },
-      result: { data },
-    }).setOnError(error => { throw error }),
-    cache: new InMemoryCache({ addTypename: false }),
+      result: { data }
+    }).setOnError(error => {
+      throw error;
+    }),
+    cache: new InMemoryCache({ addTypename: false })
   });
 }
